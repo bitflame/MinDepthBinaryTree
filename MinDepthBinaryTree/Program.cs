32d35ee9e687f04b5ee65ec868ff6516536b8e0a
Liquid Falme <sansari1@cox.net>
2019-10-04 15:30:41 -0400

    Successfully submited.

---
+++ MinDepthBinaryTree/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinDepthBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        private Int32 MinDepth(TreeNode root)
+        {
+        if (root == null)
+        {
+          return 0;
+        }
+            return FindMinDepth(root);
+        }
+        private static Int32 FindMinDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return Int32.MaxValue;
+            }
+            else if (root.left == null && root.right == null)
+            {
+                return 1;
+            }
+            else return Math.Min(FindMinDepth(root.left), FindMinDepth(root.right))+1;
+        }
+    }
+}
---
+++ MinDepthBinaryTree/TreeNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinDepthBinaryTree
+{
+    public class TreeNode
+    {
+        public Int32 val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x};
+    }
+}
